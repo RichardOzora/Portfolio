13a25e49db8ef5e6248f8e3a69c1fb32845c6019
RichardOzora <125782227+RichardOzora@users.noreply.github.com>
2023-06-02 20:00:45 +0700

    script.js

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,19 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+}
+
 function validation(){
     if(document.Formfill.Username.value==""){
         document.getElementById("result").innerHTML="Enter Username*";
