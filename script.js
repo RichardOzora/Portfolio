d55b46353da80f2f776cec1ee95f815e5fa22e85
RichardOzora <richardozora11@gmail.com>
2024-11-22 11:57:01 +0700

    changes

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//     let boxes = document.querySelectorAll('.box');
-//     boxes.forEach(function(box){
-//         let spans = [];
-//         for (let i = 0; i < 40; i++){
-//             let span = document.createElement('span');
-//             spans.push(span);
-//             box.appendChild(span);
-//             span.style.top = `${i * 1}px`;
-//             let randomDelay = (Math.random() * 0.25) + 0;
-//             span.style.transitionDelay = randomDelay + 's';
-//         }
-//         box.addEventListener('click', function(){
-//             box.classList.toggle('active');
-//         })
-//     })
-// })
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     if (window.location.pathname === "/index.html" && performance.navigation.type !== 1) {
-//         document.querySelector('.loader').style.display = 'none';
-//     }
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
     const aboutMeSection = document.getElementById('about-me');
     const aboutMeH1 = aboutMeSection.querySelector('h1');
@@ -170,6 +146,26 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+const text = document.querySelector(".sec-text");
+
+const texts = ["Front-End Developer", "UI Designer"];
+let index = 0;
+
+const textLoad = () => {
+  text.classList.add("hidden");
+  setTimeout(() => {
+    
+    text.textContent = texts[index];
+    text.classList.remove("hidden");
+
+    index = (index + 1) % texts.length;
+  }, 50);
+};
+
+textLoad();
+setInterval(textLoad, 3000);
+
 
 
 
